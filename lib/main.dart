649ed5fce36a97100edc6d8bda94589a47f86266
DeaaJahjah <deaa.jahjah@gmail.com>
2022-12-09 17:41:25 -0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:disaoled_people/app.dart';
+import 'package:disaoled_people/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
